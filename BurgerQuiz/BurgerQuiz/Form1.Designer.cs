13675be5079171327d0ed886b2e868cc34297b18
Clément Caillaud <clement.caillaud97@gmail.com>
2019-08-21 02:14:45 +0200

    Navigation dans les scènes

--- BurgerQuiz/BurgerQuiz/Form1.Designer.cs
+++ BurgerQuiz/BurgerQuiz/Form1.Designer.cs
@@ -28,21 +28,39 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(763, 486);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.ForeColor = System.Drawing.Color.Black;
+			this.BackColor = System.Drawing.SystemColors.WindowText;
+			this.ClientSize = new System.Drawing.Size(763, 486);
+			this.Controls.Add(this.pictureBox1);
+			this.Font = new System.Drawing.Font("Horseshoes and Lemonade", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.Color.White;
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Burger Quiz";
+			this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.evenement_clavier);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
 
--- BurgerQuiz/BurgerQuiz/Form1.cs
+++ BurgerQuiz/BurgerQuiz/Form1.cs
@@ -12,9 +12,62 @@ namespace BurgerQuiz
 {
 	public partial class Form1 : Form
 	{
+		private Dictionary<string, string> listeImages = new Dictionary<string, string>();
+
 		public Form1()
 		{
+			init_liste_images();
 			InitializeComponent();
+			afficher_image("score");
+		}
+
+		private void init_liste_images()
+		{
+			listeImages.Add("score", "Images/ketchupmayo_old.png");
+			listeImages.Add("nuggets", "Images/nuggets.png");
+			listeImages.Add("sel_ou_poivre", "Images/seloupoivre.png");
+			listeImages.Add("menus", "Images/menus.png");
+			listeImages.Add("addition", "Images/addition.png");
+			listeImages.Add("burger_de_la_mort", "Images/burger.png");
+		}
+
+		private void afficher_image(string key)
+		{
+			Image img = Image.FromFile(listeImages[key]);
+			pictureBox1.Image = img;
+		}
+
+		private void evenement_clavier(object sender, PreviewKeyDownEventArgs e)
+		{
+			switch (e.KeyCode)
+			{
+				case Keys.Space:
+					afficher_image("score");
+					break;
+				case Keys.N:
+					afficher_image("nuggets");
+					break;
+				case Keys.S:
+					afficher_image("sel_ou_poivre");
+					break;
+				case Keys.M:
+					afficher_image("menus");
+					break;
+				case Keys.A:
+					afficher_image("addition");
+					break;
+				case Keys.B:
+					afficher_image("burger_de_la_mort");
+					break;
+				case Keys.Up:
+					break;
+				case Keys.Down:
+					break;
+				case Keys.Right:
+					break;
+				case Keys.Left:
+					break;
+			}
 		}
 	}
 }
